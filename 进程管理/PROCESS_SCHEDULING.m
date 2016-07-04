c5478508d52eebdb1b257379b07ecd1be1ef91cd
kebinpeng <15718775@qq.com>
2016-07-04 15:00:31 +0800

    修改了进程属性的NAME

--- 进程管理/PROCESS_SCHEDULING.m
+++ 进程管理/PROCESS_SCHEDULING.m
@@ -1,3 +1,28 @@
-function PROCESS_SCHEDULING()
-    disp('ASK FOR PROCESS SCHEDULING');
+function PROCESS_SCHEDULING(PROCESS_ID,location)
+    
+    global Running_Processes_set;
+    global Ready_Processes_set;
+    global Process_Set;
+    loc=-1;
+    running_process=PROCESS_ID;
+    current_priority=Process_Set{1,location}.CURRENT_PRIORITY; 
+    
+    for i=1:255
+            if ~isempty(Ready_Processes_set{1,i})
+                loc=0;
+                for j=1:255
+                    if ~isempty(Process_Set{1,j})
+                        if Process_Set{1,j}.ID == Ready_Processes_set{1,i};
+                            loc=j;
+                            break;
+                        end
+                    end
+                end
+                if Process_Set{1,loc}.CURRENT_PRIORITY>current_priority
+                    running_process=Ready_Processes_set{1,i};
+                    current_priority=Process_Set{1,loc}.CURRENT_PRIORITY; 
+                end
+            end
+    end
+    Running_Processes_set{1,1}=running_process;
 end
