31aa31b069ee9d02f6d7dac2406ec8477148dc82
kebinpeng <15718775@qq.com>
2016-07-04 19:24:59 +0800

    更新

--- 进程管理/INVALIDE_NAME.m
+++ 进程管理/INVALIDE_NAME.m
@@ -7,19 +7,23 @@
 function [flag] = INVALIDE_NAME(STR)
 %PROCESS_ATTRIBUTE_TYPE.NAME='pkb';
 
-global Process_Set;
-global MAX_NUMBER_OF_PROCESS
-
+global PROCESS_NAME_SET;
 flag=0;
-for i=1:MAX_NUMBER_OF_PROCESS 
-%      x = Process_Set{1,i}.NAME
-% Process_NAME_Set{1,i}
-    if isempty(Process_Set{1,i}) == 0
-        if strcmp(STR,Process_Set{1,i}.NAME) == 1
-            flag = 1;
-            break;     
-        
-        end
-    end
+
+if sum( ismember(PROCESS_NAME_SET,STR) ) >=  1
+    flag = 1;
+    return;
 end
+
+% for i=1:MAX_NUMBER_OF_PROCESS 
+% %      x = Process_Set{1,i}.NAME
+% % Process_NAME_Set{1,i}
+%     if isempty(Process_Set{1,i}) == 0
+%         if strcmp(STR,Process_Set{1,i}.NAME) == 1
+%             flag = 1;
+%             break;     
+%         
+%         end
+%     end
+% end
 %disp(flag);
--- 进程管理/INVALID_ID.m
+++ 进程管理/INVALID_ID.m
@@ -1,25 +1,29 @@
 function [flag] = INVALID_ID(NUM)
 
-global Process_Set
+global PROCESS_ID_SET;
 flag=0;
-L = 0;
-for i= 1:length(Process_Set)
-    if ~isempty(Process_Set{1,i})
-        L=L+1;
-    end
+if sum( ismember(PROCESS_ID_SET,NUM) )>=1
+    flag = 1;
+    return;
 end
-
-if L == 0
-   flag = 0;
-   return;
-else
-    for i=1:L
-        
-        if Process_Set{1,i}.ID == NUM
-            flag = 1;
-            return;
-        end
-    end
-end
+% L = 0;
+% for i= 1:length(Process_Set)
+%     if ~isempty(Process_Set{1,i})
+%         L=L+1;
+%     end
+% end
+% 
+% if L == 0
+%    flag = 0;
+%    return;
+% else
+%     for i=1:L
+%         
+%         if Process_Set{1,i}.ID == NUM
+%             flag = 1;
+%             return;
+%         end
+%     end
+% end
 
 %disp(flag);
