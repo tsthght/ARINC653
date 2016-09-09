13efe8cf6a5b644865ca6f74b126548c011e982b
clwade <clwade@163.com>
2016-09-09 17:21:34 +0800

    对一些函数进行了小的改动

--- 分区内通信/事件/WAIT_EVENT.m
+++ 分区内通信/事件/WAIT_EVENT.m
@@ -2,6 +2,7 @@ function [RETURN_CODE] = WAIT_EVENT(EVENT_ID,TIME_OUT)
 
 global RETURN_CODE_TYPE;
 global Event_Set;
+global Process_Set;
 global EVENT_STATE_TYPE;
 global INFINITE_TIME_VALUE;
 global PROCESS_STATE_TYPE;
@@ -9,9 +10,11 @@ global Time_Out_Signal;
 global Current_Process;
 global Current_Partition_STATUS;
 global SYSTEM_NUMBER_OF_EVENTS;
+global SYSTEM_NUMBER_OF_PROCESSES;
 global SYSTEM_MAX_TIMEOUT;
 global Waiting_Timer_Set;
 global PROCESS_SCHEDULING_FLAG;
+global Running_Processes_set;
 
 if EVENT_ID_EXIST(EVENT_ID) == 0
     RETURN_CODE = RETURN_CODE_TYPE.INVALID_PARAM;
@@ -46,17 +49,34 @@ elseif Current_Partition_STATUS.LOCK_LEVEL ~= 0 || Current_Process.ID == 0
 
 elseif TIME_OUT == INFINITE_TIME_VALUE
     Current_Process.PROCESS_STATE = PROCESS_STATE_TYPE.WAITING;
+    PRO = Current_Process;
     INSERT_INTO_WAITING(Current_Process.ID);
+    for i = 1:SYSTEM_NUMBER_OF_PROCESSES
+        if Current_Process.ID == Process_Set{1,i}.ID
+            Process_Set{1,i} = PRO;
+        end
+    end
     INSERT_INTO_EVENT_PROCESS_QUEUE(Current_Process.ID,EVENT_ID);
+    Current_Process = [];
+    Running_Processes_set = [];
+    PROCESS_SCHEDULING_FLAG = 1;
     RETURN_CODE = RETURN_CODE_TYPE.NO_ERROR;
     return;
     
 elseif TIME_OUT >0 && Event_Set{1,index}.STATE == EVENT_STATE_TYPE.DOWN
     Current_Process.PROCESS_STATE = PROCESS_STATE_TYPE.WAITING;
+    PRO = Current_Process;
     INSERT_INTO_WAITING(Current_Process.ID);
+    for i = 1:SYSTEM_NUMBER_OF_PROCESSES
+        if Current_Process.ID == Process_Set{1,i}.ID
+            Process_Set{1,i} = PRO;
+        end
+    end
     INSERT_INTO_EVENT_PROCESS_QUEUE(Current_Process.ID,EVENT_ID);
     TIME_COUNTER = struct('PROCESS_ID',Current_Process.ID,'EVENT_ID',EVENT_ID,'DURATION',TIME_OUT,'TOS',round(rand(1)));
     Waiting_Timer_Set{1,numel(Waiting_Timer_Set)+1} = TIME_COUNTER;
+    Current_Process = [];
+    Running_Processes_set = [];
     PROCESS_SCHEDULING_FLAG = 1;
     if TIME_COUNTER.TOS == Time_Out_Signal.TRUE
         RETURN_CODE = RETURN_CODE_TYPE.TIMED_OUT;
